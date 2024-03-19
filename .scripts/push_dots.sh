80d8e6b4f22949cf72194e8eae3a2d1a3b5da5b6
Moritz <moritz.huber@protonmail.com>
2024-03-19 17:42:05 +0100

    c

--- .scripts/push_dots.sh
+++ .scripts/push_dots.sh
@@ -1,3 +1,5 @@
-!#/bin/bash
+#!/bin/zsh
 
-dots 
+/usr/bin/git --git-dir=/Users/moritz/.dots/ --work-tree=/Users/moritz add .
+/usr/bin/git --git-dir=/Users/moritz/.dots/ --work-tree=/Users/moritz commit -m "c"
+/usr/bin/git --git-dir=/Users/moritz/.dots/ --work-tree=/Users/moritz push
